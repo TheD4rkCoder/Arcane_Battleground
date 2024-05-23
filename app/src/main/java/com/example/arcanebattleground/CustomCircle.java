8f8c7f481c0958bbcdd2f53b6fccba992907d1a7
Fubyx <fabireif11@gmail.com>
2024-05-23 22:36:12 +0200

    Erklärung auf Whatsapp

---
+++ app/src/main/java/com/example/arcanebattleground/CustomCircle.java
@@ -0,0 +1,13 @@
+package com.example.arcanebattleground;
+
+public class CustomCircle {
+
+    public float centerX;
+    public float centerY;
+    public float radius;
+    public CustomCircle(float centerX, float centerY, float radius){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+}
