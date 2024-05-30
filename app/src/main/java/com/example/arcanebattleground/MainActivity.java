2e9423d887c01dfd8fc8e7778dec7a3d6cf6da0e
David Mairhofer <david2mairhofer@gmail.com>
2024-05-30 12:38:30 +0200

    main menu

--- app/src/main/java/com/example/arcanebattleground/MainActivity.java
+++ app/src/main/java/com/example/arcanebattleground/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainActivity = this;
-        setContentView(new GameView(this));
+        setContentView(new MainMenu(this));
     }
 }
--- app/src/main/java/com/example/arcanebattleground/MainMenu.java
+++ app/src/main/java/com/example/arcanebattleground/MainMenu.java
@@ -1,7 +1,10 @@
 package com.example.arcanebattleground;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -9,34 +12,50 @@ import android.view.SurfaceView;
 import androidx.annotation.NonNull;
 
 public class MainMenu extends SurfaceView implements SurfaceHolder.Callback {
-    /*
-    Intent intent = new Intent(MainActivity.mainActivity, GameActivity.class);
-    MainActivity.startActivity(intent);
-     */
     private static SurfaceHolder holder;
+    private Paint paintForTexts;
+    private int screenWidth, screenHeight;
 
     public MainMenu(Context context) {
         super(context);
         holder = getHolder();
         holder.addCallback(this);
 
+        screenWidth = getResources().getDisplayMetrics().widthPixels;
+        screenHeight = getResources().getDisplayMetrics().heightPixels;
+
+        paintForTexts = new Paint();
+        paintForTexts.setColor(Color.RED);
+        paintForTexts.setTextSize(screenWidth * 0.1f);
+        paintForTexts.setTypeface(getResources().getFont(R.font.dpcomic));
+        paintForTexts.setTextAlign(Paint.Align.CENTER);
     }
 
-    public static void drawMenu() {
+    public void drawMenu() {
         //should always be first:
         Canvas c = holder.lockCanvas();
+        // TODO: draw background
+
+        // TODO: field for entering id, ect
+        c.drawText("Offline Play", screenWidth * 0.5f, screenHeight * 0.2f, paintForTexts);
+
+        c.drawText("Create lobby", screenWidth * 0.5f, screenHeight * 0.4f, paintForTexts);
+
+        c.drawText("Join lobby", screenWidth * 0.5f, screenHeight * 0.6f, paintForTexts);
 
         // should always be last:
         holder.unlockCanvasAndPost(c);
     }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // do smth different for online i guess
-        if (Animation.isAnimationPlaying())
-            return true;
-        boolean endMove = false;
         if (event.getAction() == MotionEvent.ACTION_UP) {
-
+            if (event.getY() > screenHeight * 0.2 - paintForTexts.getTextSize() && event.getY() < screenHeight * 0.2) {
+                Intent intent = new Intent(MainActivity.mainActivity, GameActivity.class);
+                MainActivity.mainActivity.startActivity(intent);
+            } else if (event.getY() > screenHeight * 0.4- paintForTexts.getTextSize() && event.getY() < screenHeight * 0.4) {
+            } else if (event.getY() > screenHeight * 0.6- paintForTexts.getTextSize() && event.getY() < screenHeight * 0.6) {
+            }
         }
 
         return true; // touch already handled (for event bubbling)
